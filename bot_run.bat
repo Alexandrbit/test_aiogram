e382f79780d1a3ae7f311bac7a06bf31ff9dce93
Alexandrbit <rianimator@gmail.com.ua>
2022-08-19 19:23:09 +0300

    over

---
+++ bot_run.bat
@@ -0,0 +1,11 @@
+@echo off
+
+call %`dp0E:\PyCharm\test_aiogram\venv\Scripts\activate
+
+cd %`dpE:\PyCharm\test_aiogram
+
+set TOKEN=5567449046:AAEj1_nS_nAtNQc6UZDB-p0aLLbjrepOLuI
+
+python bot.py
+
+pause
